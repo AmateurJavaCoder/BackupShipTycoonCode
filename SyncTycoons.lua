516427f773154a198a418ccd90ce06c0cca551ed
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-10-19 09:15:35 +1100

    Update SyncTycoons.lua

--- SyncTycoons.lua
+++ SyncTycoons.lua
@@ -1,9 +1,12 @@
-for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
-	local clone = game.Workspace["Ship Tycoon 2"].Tycoons["Bright blue"]:Clone()
-	clone.Parent = game.Workspace["Ship Tycoon 2"].Tycoons
-	clone:PivotTo(tycoon:GetPivot())
-	clone:WaitForChild("TeamColor").Value = tycoon:WaitForChild("TeamColor").Value
-	clone.Name = tycoon.Name
-	tycoon:Destroy()
-	print("Successfully synced tycoon: "..clone.Name)
+for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do
+	if tycoon.Name ~= "Bright blue" then
+		local clone = game.Workspace["Ship Tycoon 2"].Tycoons["Bright blue"]:Clone()
+		clone.Parent = game.Workspace["Ship Tycoon 2"].Tycoons
+		clone:PivotTo(tycoon:GetPivot())
+		clone:WaitForChild("TeamColor").Value = tycoon:WaitForChild("TeamColor").Value
+		clone.Essentials.Spawn.BrickColor = tycoon.TeamColor.Value
+		clone.Name = tycoon.Name
+		tycoon:Destroy()
+		print("Successfully synced tycoon: "..clone.Name)
+	end
 end
