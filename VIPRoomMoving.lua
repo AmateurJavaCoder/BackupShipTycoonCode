ebeff7e1d4dc7a3f52b7cba4e0cc8225343e5c9a
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-10-20 15:12:36 +1100

    Update VIPRoomMoving.lua

--- VIPRoomMoving.lua
+++ VIPRoomMoving.lua
@@ -1,22 +1,26 @@
-local offset = 100
-for _, tycoons in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
+local offset = Vector3.new(9,0,3)
+
+local root = game.Workspace["Ship Tycoon 2"].Tycoons
+local tycoon = {root["Bright blue"]}
+for _, tycoons in pairs(tycoon or game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
     for _, purchase in pairs(tycoons.Purchases:GetChildren()) do
-        if string.find(string.upper(purchase.Name), "VIP") or string.find(string.upper(purchase.Name), "BEDROCK") or string.find(string.upper(purchase.Name), "AZURITE") or string.find(string.upper(purchase.Name), "ORPIMENT") or string.find(string.upper(purchase.Name), "TURBO") or string.find(string.upper(purchase.Name), "ENHANCED") then 
+        if string.find(string.upper(purchase.Name), "VIP") or string.find(string.upper(purchase.Name), "BEDROCK") or string.find(string.upper(purchase.Name), "AZURITE") or string.find(string.upper(purchase.Name), "ORPIMENT") or string.find(string.upper(purchase.Name), "TURBO") or string.find(string.upper(purchase.Name), "ENHANCED") or string.find(string.upper(purchase.Name), "PREMIUM") then 
             for i, v in pairs(purchase:GetDescendants() ) do
-                if v:IsA("BasePart") or v:IsA("UnionOperation") then v.Position += Vector3.new(0,offset,0) 
+                if v:IsA("BasePart") or v:IsA("UnionOperation") then 
+                    v.Position += offset 
                 end 
             end 
-        end 
+        end
     end 
     for _, purchase in pairs(tycoons.Buttons:GetChildren()) do 
-        if string.find(string.upper(purchase.Name), "VIP") or string.find(string.upper(purchase.Name), "BEDROCK") or string.find(string.upper(purchase.Name), "AZURITE") or string.find(string.upper(purchase.Name), "ORPIMENT") or string.find(string.upper(purchase.Name), "TURBO") or string.find(string.upper(purchase.Name), "ENHANCED") then 
-            purchase.Head.Position += Vector3.new(0,offset,0)
+        if string.find(string.upper(purchase.Name), "VIP") or string.find(string.upper(purchase.Name), "BEDROCK") or string.find(string.upper(purchase.Name), "AZURITE") or string.find(string.upper(purchase.Name), "ORPIMENT") or string.find(string.upper(purchase.Name), "TURBO") or string.find(string.upper(purchase.Name), "ENHANCED") or string.find(string.upper(purchase.Name), "PREMIUM") then 
+            purchase.Head.Position += offset
         end 
     end 
 
-    local extra = {"OrpimentCon", "BedrockCon", "AzuriteCon"}
+    local extra = {"OrpimentCon", "BedrockCon", "AzuriteCon", "PremiumCon"}
     for i, v in pairs(extra) do
-        tycoons.Purchases:FindFirstChild(v).Position += Vector3.new(0,offset,0)
+        tycoons.Purchases:FindFirstChild(v).Position += offset
     end
 
 end
