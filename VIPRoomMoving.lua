d95702eca17e934f7c9a66b695d87f4b888ecf03
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-10-20 15:18:46 +1100

    Update VIPRoomMoving.lua

--- VIPRoomMoving.lua
+++ VIPRoomMoving.lua
@@ -1,8 +1,9 @@
 local offset = Vector3.new(9,0,3)
 
 local root = game.Workspace["Ship Tycoon 2"].Tycoons
-local tycoon = {root["Bright blue"]}
-for _, tycoons in pairs(tycoon or game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
+local tycoons = {root["Bright blue"]}
+
+local function move(tycoons) 
     for _, purchase in pairs(tycoons.Purchases:GetChildren()) do
         if string.find(string.upper(purchase.Name), "VIP") or string.find(string.upper(purchase.Name), "BEDROCK") or string.find(string.upper(purchase.Name), "AZURITE") or string.find(string.upper(purchase.Name), "ORPIMENT") or string.find(string.upper(purchase.Name), "TURBO") or string.find(string.upper(purchase.Name), "ENHANCED") or string.find(string.upper(purchase.Name), "PREMIUM") then 
             for i, v in pairs(purchase:GetDescendants() ) do
@@ -22,5 +23,14 @@ for _, tycoons in pairs(tycoon or game.Workspace["Ship Tycoon 2"].Tycoons:GetChi
     for i, v in pairs(extra) do
         tycoons.Purchases:FindFirstChild(v).Position += offset
     end
+end
 
+if #tycoons > 0 then
+    for _, tycoon in pairs(tycoons) do
+       move(tycoon) 
+    end
+else
+  for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
+        move(tycoon)
+  end  
 end
