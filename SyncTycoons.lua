08cf3db230323ac105e427f95e8858361ebf47f1
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-10-23 17:28:56 +1100

    Update SyncTycoons.lua

--- SyncTycoons.lua
+++ SyncTycoons.lua
@@ -6,6 +6,8 @@ for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do
 		clone:WaitForChild("TeamColor").Value = tycoon:WaitForChild("TeamColor").Value
 		clone.Essentials.Spawn.BrickColor = tycoon.TeamColor.Value
 		clone.Purchases.VIPRoom.Color.BrickColor = tycoon.TeamColor.Value
+		clone.Entrance.Part1.BrickColor = tycoon.TeamColor.Value
+		clone.Entrance.Part2.BrickColor = tycoon.TeamColor.Value
 		clone.Name = tycoon.Name
 		tycoon:Destroy()
 		print("Successfully synced tycoon: "..clone.Name)
