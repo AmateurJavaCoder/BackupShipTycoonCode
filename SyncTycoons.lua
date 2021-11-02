c235008a5e85d43ba468959bd4e370eec93ffdcb
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-11-02 22:25:27 +1100

    Update SyncTycoons.lua

--- SyncTycoons.lua
+++ SyncTycoons.lua
@@ -1,15 +1,17 @@
-for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do
-	if tycoon.Name ~= "Bright blue" then
-		local clone = game.Workspace["Ship Tycoon 2"].Tycoons["Bright blue"]:Clone()
-		clone.Parent = game.Workspace["Ship Tycoon 2"].Tycoons
-		clone:PivotTo(tycoon:GetPivot())
-		clone:WaitForChild("TeamColor").Value = tycoon:WaitForChild("TeamColor").Value
-		clone.Essentials.Spawn.BrickColor = tycoon.TeamColor.Value
-		clone.Purchases.VIPRoom.Color.BrickColor = tycoon.TeamColor.Value
-		clone.Entrance.Part1.BrickColor = tycoon.TeamColor.Value
-		clone.Entrance.Part2.BrickColor = tycoon.TeamColor.Value
-		clone.Name = tycoon.Name
-		tycoon:Destroy()
-		print("Successfully synced tycoon: "..clone.Name)
+function syncTycoons()
+	for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do
+		if tycoon.Name ~= "Bright blue" then
+			local clone = game.Workspace["Ship Tycoon 2"].Tycoons["Bright blue"]:Clone()
+			clone.Parent = game.Workspace["Ship Tycoon 2"].Tycoons
+			clone:PivotTo(tycoon:GetPivot())
+			clone:WaitForChild("TeamColor").Value = tycoon:WaitForChild("TeamColor").Value
+			clone.Essentials.Spawn.BrickColor = tycoon.TeamColor.Value
+			clone.Purchases.VIPRoom.Color.BrickColor = tycoon.TeamColor.Value
+			clone.Entrance.Part1.BrickColor = tycoon.TeamColor.Value
+			clone.Entrance.Part2.BrickColor = tycoon.TeamColor.Value
+			clone.Name = tycoon.Name
+			tycoon:Destroy()
+			print("Successfully synced tycoon: "..clone.Name)
+		end
 	end
 end
