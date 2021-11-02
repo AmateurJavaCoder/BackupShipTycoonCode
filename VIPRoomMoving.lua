d5cca94f8347f4ceae9d27e1deec24d81ff52fd6
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-11-02 22:24:57 +1100

    Update VIPRoomMoving.lua

--- VIPRoomMoving.lua
+++ VIPRoomMoving.lua
@@ -1,54 +1,54 @@
 function moveVIPRooms(offset1, offset2, offset3)
-local offset = Vector3.New(offset1 or 0, offset2 or 0, offset 3 or 0) 
-   
+	local offset = Vector3.New(offset1 or 0, offset2 or 0, offset3 or 0) 
 
-local root = game.Workspace["Ship Tycoon 2"].Tycoons
-local selective = false
-local tycoons = {root["Bright blue"]}
 
-local searchTerms = {"VIP", "Bedrock", "Azurite", "Orpiment", "Turbo", "Premium", "Enhanced", "Elite"}
-local extra = {"OrpimentCon", "BedrockCon", "AzuriteCon", "PremiumCon", "EliteCon"}
-    
-local function search(object)
-    for _, term in pairs(searchTerms) do 
-       if string.find(string.upper(object.Name), string.upper(term)) then
-            return true
-       end
-    end
-    return false
+	local root = game.Workspace["Ship Tycoon 2"].Tycoons
+	local selective = false
+	local tycoons = {root["Bright blue"]}
+
+	local searchTerms = {"VIP", "Bedrock", "Azurite", "Orpiment", "Turbo", "Premium", "Enhanced", "Elite"}
+	local extra = {"OrpimentCon", "BedrockCon", "AzuriteCon", "PremiumCon", "EliteCon"}
+
+	local function search(object)
+		for _, term in pairs(searchTerms) do 
+			if string.find(string.upper(object.Name), string.upper(term)) then
+				return true
+			end
+		end
+		return false
+	end
+
+	local function move(tycoons) 
+		for _, purchase in pairs(tycoons.Purchases:GetChildren()) do
+			if search(purchase) then 
+				for i, v in pairs(purchase:GetDescendants() ) do
+					if v:IsA("BasePart") or v:IsA("UnionOperation") then 
+						v.Position += offset 
+					end 
+				end 
+			end
+		end 
+		for _, purchase in pairs(tycoons.Buttons:GetChildren()) do 
+			if search(purchase) then 
+				purchase.Head.Position += offset
+			end 
+		end 
+
+
+		for i, v in pairs(extra) do
+			tycoons.Purchases:FindFirstChild(v).Position += offset
+		end
+	end
+
+	if selective and #tycoons > 0 then
+		for _, tycoon in pairs(tycoons) do
+			move(tycoon) 
+			print(tycoon.Name)
+		end
+	else
+		for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
+			move(tycoon)
+			print(tycoon.Name)
+		end  
+	end
 end
-    
-local function move(tycoons) 
-    for _, purchase in pairs(tycoons.Purchases:GetChildren()) do
-        if search(purchase) then 
-            for i, v in pairs(purchase:GetDescendants() ) do
-                if v:IsA("BasePart") or v:IsA("UnionOperation") then 
-                    v.Position += offset 
-                end 
-            end 
-        end
-    end 
-    for _, purchase in pairs(tycoons.Buttons:GetChildren()) do 
-        if search(purchase) then 
-            purchase.Head.Position += offset
-        end 
-    end 
-
-    
-    for i, v in pairs(extra) do
-        tycoons.Purchases:FindFirstChild(v).Position += offset
-    end
-end
-
-if selective and #tycoons > 0 then
-    for _, tycoon in pairs(tycoons) do
-       move(tycoon) 
-       print(tycoon.Name)
-    end
-else
-  for _, tycoon in pairs(game.Workspace["Ship Tycoon 2"].Tycoons:GetChildren()) do 
-        move(tycoon)
-        print(tycoon.Name)
-  end  
-end
-    end
