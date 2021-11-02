cf3ee5db021e35bbf593fd66c1c7508c0c2f18f0
AmateurJavaCoder <58548180+AmateurJavaCoder@users.noreply.github.com>
2021-11-02 22:19:08 +1100

    Update ClearFeedback.lua

--- ClearFeedback.lua
+++ ClearFeedback.lua
@@ -1,3 +1,11 @@
 local function clearFeedback()
+  local DSS = game:GetService("DataStoreService")
+  local BugReports = DSS:GetDataStore("BugReports")
+  local Feedback = DSS:GetDataStore("Feedback")
+  
+  Feedback:RemoveAsync("Value")
+  print("Cleared Feedback")
+  BugReports:RemoveAsync("Value")
+  print("Cleared Bug Reports")
   
 end
